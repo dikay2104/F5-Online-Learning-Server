dcc5c746817d05f1096c2ba0648a4f62bcc476c9
ngthanhdat-28 <89628604+ngthanhdat-28@users.noreply.github.com>
2025-07-09 23:08:45 +0700

    update courseController userController User.js

--- src/models/User.js
+++ src/models/User.js
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema({
   password: String,
   role: { type: String, enum: ['student', 'teacher', 'admin'], default: 'student' },
   avatar: String,
-  isActive: { type: Boolean, default: true }
+  isActive: { type: Boolean, default: true },
+  isBanned: { type: Boolean, default: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
