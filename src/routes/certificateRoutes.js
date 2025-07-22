4b44ebdc2da9b98dde708017be7ee40ceacb1d6a
hine2110 <huyntgde170695@fpt.edu.vn>
2025-07-22 21:23:11 +0700

    thêm certificate

---
+++ src/routes/certificateRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const certificateController = require('../controllers/certificateController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+router.post('/issue', authMiddleware.verifyToken, certificateController.issueCertificate);
+router.get('/my', authMiddleware.verifyToken, certificateController.getMyCertificates);
+router.get('/:certificateId', authMiddleware.verifyToken, certificateController.getCertificateById);
+
+module.exports = router; 
